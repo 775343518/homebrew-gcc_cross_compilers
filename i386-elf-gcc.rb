11b1f123aa655affa55791961ebffaebc7b6e375
lanhao <wolanhao@outlook.com>
2017-07-12 23:17:18 +0800

    gcc7.1

--- i386-elf-gcc.rb
+++ i386-elf-gcc.rb
@@ -15,10 +15,10 @@ class I386ElfGcc < Formula
   def install
     binutils = Formula.factory 'i386-elf-binutils'
 
-    ENV['CC'] = '/usr/local/bin/gcc-7'
-    ENV['CXX'] = '/usr/local/bin/g++-7'
-    ENV['CPP'] = '/usr/local/bin/cpp-7'
-    ENV['LD'] = '/usr/local/bin/gcc-7'
+    ENV['CC'] = '/usr/local/bin/gcc-7.1'
+    ENV['CXX'] = '/usr/local/bin/g++-7.1'
+    ENV['CPP'] = '/usr/local/bin/cpp-7.1'
+    ENV['LD'] = '/usr/local/bin/gcc-7.1'
     ENV['PATH'] += ":#{binutils.prefix/"bin"}"
 
     mkdir 'build' do
