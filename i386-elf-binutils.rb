7a56491e440ed209ae13b6dd34817591850d6b24
lanhao <wolanhao@outlook.com>
2017-07-12 23:25:45 +0800

    gcc7.1

--- i386-elf-binutils.rb
+++ i386-elf-binutils.rb
@@ -8,10 +8,10 @@ class I386ElfBinutils < Formula
   depends_on 'gcc7' => :build
 
   def install
-    ENV['CC'] = '/usr/local/bin/gcc-7'
-    ENV['CXX'] = '/usr/local/bin/g++-7'
-    ENV['CPP'] = '/usr/local/bin/cpp-7'
-    ENV['LD'] = '/usr/local/bin/gcc-7'
+    ENV['CC'] = '/usr/local/bin/gcc-7.1'
+    ENV['CXX'] = '/usr/local/bin/g++-7.1'
+    ENV['CPP'] = '/usr/local/bin/cpp-7.1'
+    ENV['LD'] = '/usr/local/bin/gcc-7.1'
 
     mkdir 'build' do
       system '../configure', '--disable-nls', '--target=i386-elf',
